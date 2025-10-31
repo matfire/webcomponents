177904172a201bedc97c1b4c9b514ded076bbc72
Matteo Gassend <mgassend@gmail.com>
2025-10-31 20:00:51 +0100

    new release

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import Codesandbox from "./components/codesandbox"
 import Stackblitz from "./components/stackblitz"
 import Youtube from "./components/youtube"
 
-customElements.define('stackblitz', Stackblitz)
-customElements.define("callout", Callout)
-customElements.define('codesandbox', Codesandbox)
-customElements.define('youtube', Youtube)
+customElements.define('wc-stackblitz', Stackblitz)
+customElements.define('wc-callout', Callout)
+customElements.define('wc-codesandbox', Codesandbox)
+customElements.define('wc-youtube', Youtube)
